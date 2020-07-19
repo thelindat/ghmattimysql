f6aa62da8ee4d7f1e7739748f60ed6ccb2d6dd0d
GHMatti <33543464+GHMatti@users.noreply.github.com>
2020-07-19 16:21:44 +0200

    chore: version

--- packages/ghmattimysql/src/static/fxmanifest.lua
+++ packages/ghmattimysql/src/static/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'common'
 name 'ghmattimysql'
 description 'MySQL Middleware for fivem using mysql.js.'
 author 'Matthias Mandelartz'
-version '1.3.1'
+version '1.3.2'
 url 'https://github.com/GHMatti/ghmattimysql'
 
 server_scripts {
